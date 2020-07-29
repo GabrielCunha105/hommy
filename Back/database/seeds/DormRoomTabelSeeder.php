0fb122ddd04afe4f15a9700da60f9ad3ff512830
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-07-29 19:03:27 -0300

    +relação de favoritos no seeder

--- Back/database/seeds/DormRoomTabelSeeder.php
+++ Back/database/seeds/DormRoomTabelSeeder.php
@@ -14,6 +14,9 @@ class DormRoomTabelSeeder extends Seeder
         factory (App\DormRoom::class,12)->create()->each(function ($dormRoom) {
             $comments = factory (App\Comment::class, 2)->make();
             $dormRoom->comments()->saveMany($comments);
+            $user = factory (App\User::class)->make();
+            $user->save();
+            $user->favoritas()->attach($dormRoom);
         });
     }
 }
